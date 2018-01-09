8d5e517336d2290ea4ec15b1e1f834b2c9188da9
Ian Turton <ijturton@gmail.com>
2018-01-09 17:15:18 +0000

    improvements to support stupid microsoft log file format

--- src/main/java/com/astuntechnology/utilities/ExtractJmeterParams.java
+++ src/main/java/com/astuntechnology/utilities/ExtractJmeterParams.java
@@ -8,6 +8,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.URLDecoder;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -17,6 +18,7 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -28,6 +30,9 @@ import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 
 public class ExtractJmeterParams {
+	enum TYPE {
+		APACHE, MICROSOFT;
+	}
 
 	/**
 	 * Process an access log file to create input for a JMeter session
@@ -39,8 +44,10 @@ public class ExtractJmeterParams {
 
 	private ArrayList<String> columns = new ArrayList<>();
 	private HashMap<Calendar, HashMap<String, String>> results = new HashMap<>();
-	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/YYYY:HH:mm:ssZ");
+	private static SimpleDateFormat asdf = new SimpleDateFormat("dd/MMM/YYYY:HH:mm:ssZ");
+	private static SimpleDateFormat msdf = new SimpleDateFormat("dd-MM-YYYY HH:mm:ss");
 	private String seperator = "\t";
+	private TYPE type = TYPE.APACHE;
 
 	public static void main(String[] args) throws IOException, ParseException, org.apache.commons.cli.ParseException {
 		Options options = new Options();
@@ -50,10 +57,14 @@ public class ExtractJmeterParams {
 				.desc("space seperated list of parameters to extract from the url").build());
 		options.addOption(Option.builder("s").longOpt("separator").hasArg()
 				.desc("character to separate output columns (default tab)").build());
+		options.addOption(Option.builder("m").longOpt("microsoft").desc("Is this a microsoft log file").build());
 		CommandLineParser parser = new DefaultParser();
 		CommandLine cmd = parser.parse(options, args);
 		ExtractJmeterParams me = new ExtractJmeterParams();
 
+		if (cmd.hasOption("m")) {
+			me.setType(TYPE.MICROSOFT);
+		}
 		String[] cols = cmd.getOptionValues("c");
 
 		String inputFile = cmd.getOptionValue("f");
@@ -72,8 +83,8 @@ public class ExtractJmeterParams {
 			me.setColumns(cols);
 			me.parseFile(inputFile);
 			// me.print("REQUEST","getMap",out);
-			//me.print("URL", "wms", out);
-			me.print( out);
+			// me.print("URL", "wms", out);
+			me.print(out);
 		} finally {
 			if (out != System.out) {
 				out.close();
@@ -84,7 +95,9 @@ public class ExtractJmeterParams {
 
 	public void setColumns(String[] strings) {
 		columns = new ArrayList<>();
-		columns.addAll(Arrays.asList(strings));
+		for (String col : strings) {
+			columns.add(col.toUpperCase());
+		}
 	}
 
 	public String[] getColumns() {
@@ -99,29 +112,48 @@ public class ExtractJmeterParams {
 
 		try (BufferedReader reader = new BufferedReader(new FileReader(new File(inputFile)))) {
 			String line;
-
-			Pattern pURL = Pattern.compile("^.*\"GET (.+) HTTP.*$");
-			Pattern pTime = Pattern.compile("^.*\\[(.+)\\].*$");
+			Pattern pURL;
+			Pattern pTime;
+			DateFormat sdf;
+			if (type.equals(TYPE.APACHE)) {
+				pURL = Pattern.compile("^.*\"GET (.+) HTTP.*$");
+				pTime = Pattern.compile("^.*\\[(.+)\\].*$");
+				sdf = asdf;
+			} else {
+				pURL = Pattern.compile("^.*GET (.+) (.+) \\d{3,4} .*$");
+				// 2017-01-13 08:34:18
+				pTime = Pattern.compile("^([\\d-]+ [\\d:]+) .*$");
+				sdf = msdf;
+			}
 
 			while ((line = reader.readLine()) != null) {
+				if (line.startsWith("#")) {
+					// Microsoft IIS includes "comments" in the file!
+					continue;
+				}
 				HashMap<String, String> params = new HashMap<>();
+
 				Matcher d = pTime.matcher(line);
 				Calendar date = Calendar.getInstance();
 				if (d.matches()) {
-					date.setTime(sdf.parse(d.group(1).replaceAll(" ", "")));
+
+					String dateString = d.group(1);
+					if (type.equals(TYPE.APACHE))
+						dateString = dateString.replaceAll(" ", "");
+					date.setTime(sdf.parse(dateString));
 				}
 				Matcher m = pURL.matcher(line);
 				if (m.matches()) {
 					String query = URLDecoder.decode(m.group(1), "UTF-8");
 					// System.out.println(query);
-					String[] parts = query.split("&|;|\\?");
-					boolean first=true;
+					String[] parts = query.split("&|;|\\?| ");
+					boolean first = true;
 					for (String part : parts) {
-						if(first) {
-							first=false;
+						if (first) {
+							first = false;
 							params.put("URL", part);
-							if(!columns.contains("URL")) {
-								columns.add(0,"URL");
+							if (!columns.contains("URL")) {
+								columns.add(0, "URL");
 							}
 						}
 						String[] bits = part.split("=");
@@ -131,7 +163,9 @@ public class ExtractJmeterParams {
 							params.put(bits[0], null);
 						}
 					}
-					while(results.containsKey(date)) {//separate the requests (assume no more than 1000 reqs per s)
+					while (results.containsKey(date)) {// separate the requests
+														// (assume no more than
+														// 1000 reqs per s)
 						date.add(Calendar.MILLISECOND, 1);
 					}
 					results.put(date, params);
@@ -144,7 +178,7 @@ public class ExtractJmeterParams {
 	public void print() {
 		print(null, null, System.out);
 	}
-	
+
 	public void print(PrintStream out) {
 		print(null, null, out);
 	}
@@ -168,7 +202,7 @@ public class ExtractJmeterParams {
 					if (params.containsKey(colm) && params.get(colm).toLowerCase().contains(val)) {
 						for (String col : getColumns()) {
 							if (params.containsKey(col)) {
-								out.print(params.get(col) + seperator); 
+								out.print(params.get(col) + seperator);
 							} else {
 								out.print(seperator);
 							}
@@ -209,4 +243,15 @@ public class ExtractJmeterParams {
 	public void setSeperator(String seperator) {
 		this.seperator = seperator;
 	}
+
+	public TYPE getType() {
+		return type;
+	}
+
+	public void setType(TYPE type) {
+		this.type = type;
+	}
+
+	
+
 }
--- src/test/java/com/astuntechnology/utilities/ExtractJmeterParamsTest.java
+++ src/test/java/com/astuntechnology/utilities/ExtractJmeterParamsTest.java
@@ -21,22 +21,27 @@ import java.util.HashMap;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.astuntechnology.utilities.ExtractJmeterParams.TYPE;
+
 public class ExtractJmeterParamsTest {
-	static File good;
+	private static File good;
+	private static File ms;
+
 	@BeforeClass
 	static public void setup() {
 		good = urlToFile(ExtractJmeterParamsTest.class.getResource("test.txt"));
+		ms = urlToFile(ExtractJmeterParamsTest.class.getResource("iss-log.log"));
 	}
+
 	@Test
 	public void testSetColumns() {
 		ExtractJmeterParams extractor = new ExtractJmeterParams();
-		String[] expected = new String[]{"ian","fred","bert"};
-		extractor.setColumns(expected);
+		String[] expects = new String[] { "ian", "fred", "bert" };
+		extractor.setColumns(expects);
 		String[] observed = extractor.getColumns();
+		String[] expected = new String[] { "IAN", "FRED", "BERT" };
 		assertArrayEquals(expected, observed);
 	}
-
-
 
 	@Test
 	public void testGetResults() throws FileNotFoundException, IOException, ParseException {
@@ -50,15 +55,15 @@ public class ExtractJmeterParamsTest {
 	public void testParseFile() throws FileNotFoundException, IOException, ParseException {
 		ExtractJmeterParams extractor = new ExtractJmeterParams();
 		extractor.parseFile(good.getAbsolutePath());
-		
+
 	}
 
 	@Test
 	public void testSetSeperator() throws FileNotFoundException, IOException, ParseException {
 		ExtractJmeterParams extractor = new ExtractJmeterParams();
-		
+
 		extractor.setSeperator(";");
-		extractor.setColumns(new String[] {"LAYERS","HEIGHT","WIDTH","BBOX"});
+		extractor.setColumns(new String[] { "LAYERS", "HEIGHT", "WIDTH", "BBOX" });
 		String obs = extractor.getSeperator();
 		assertEquals(";", obs);
 		extractor.parseFile(good.getAbsolutePath());
@@ -67,84 +72,125 @@ public class ExtractJmeterParamsTest {
 		extractor.print(out);
 		byte[] b = bytes.toByteArray();
 		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(b);
-		BufferedReader r = new BufferedReader(new InputStreamReader(byteArrayInputStream),32);
+		BufferedReader r = new BufferedReader(new InputStreamReader(byteArrayInputStream), 32);
 		String header = r.readLine();
 		assertEquals("URL;LAYERS;HEIGHT;WIDTH;BBOX;", header);
-		extractor.setColumns(new String[] {"LAYERS","HEIGHT","WIDTH","BBOX","URL"});
+		extractor.setColumns(new String[] { "LAYERS", "HEIGHT", "WIDTH", "BBOX", "URL" });
 		extractor.parseFile(good.getAbsolutePath());
-		 bytes = new ByteArrayOutputStream();
-		 out = new PrintStream(bytes);
+		bytes = new ByteArrayOutputStream();
+		out = new PrintStream(bytes);
 		extractor.print(out);
 		b = bytes.toByteArray();
 		byteArrayInputStream = new ByteArrayInputStream(b);
-		r = new BufferedReader(new InputStreamReader(byteArrayInputStream),32);
+		r = new BufferedReader(new InputStreamReader(byteArrayInputStream), 32);
 		header = r.readLine();
 		assertEquals("LAYERS;HEIGHT;WIDTH;BBOX;URL;", header);
-		
+
 	}
-    /**
-     * Takes a URL and converts it to a File. The attempts to deal with Windows UNC format specific problems, specifically files located on network
-     * shares and different drives.
-     * <p>
-     * If the URL.getAuthority() returns null or is empty, then only the url's path property is used to construct the file. Otherwise, the authority
-     * is prefixed before the path.
-     * <p>
-     * It is assumed that url.getProtocol returns "file".
-     * <p>
-     * Authority is the drive or network share the file is located on. Such as "C:", "E:", "\\fooServer"
-     * 
-     * @param url a URL object that uses protocol "file"
-     * @return a File that corresponds to the URL's location
-     */
-    public static File urlToFile(URL url) {
-        if (!"file".equals(url.getProtocol())) {
-            return null; // not a File URL
-        }
-        String string = url.toString();
-        if (url.getQuery() != null) {
-            string = string.substring(0, string.indexOf("?"));
-        }
-        if (string.contains("+")) {
-            // this represents an invalid URL created using either
-            // file.toURL(); or
-            // file.toURI().toURL() on a specific version of Java 5 on Mac
-            string = string.replace("+", "%2B");
-        }
-        try {
-            string = URLDecoder.decode(string, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException("Could not decode the URL to UTF-8 format", e);
-        }
-        String path3;
-        String simplePrefix = "file:/";
-        String standardPrefix = "file://";
-        if (IS_WINDOWS_OS && string.startsWith(standardPrefix)) {
-            // win32: host/share reference. Keep the host slashes.
-            path3 = string.substring(standardPrefix.length() - 2);
-            File f = new File(path3);
-            if (!f.exists()) {
-                // Make path relative to be backwards compatible.
-                path3 = path3.substring(2, path3.length());
-            }
-        } else if (string.startsWith(standardPrefix)) {
-            path3 = string.substring(standardPrefix.length());
-        } else if (string.startsWith(simplePrefix)) {
-            path3 = string.substring(simplePrefix.length() - 1);
-        } else {
-            String auth = url.getAuthority();
-            String path2 = url.getPath().replace("%20", " ");
-            if (auth != null && !auth.equals("")) {
-                path3 = "//" + auth + path2;
-            } else {
-                path3 = path2;
-            }
-        }
-        return new File(path3);
-    }
-    /**
-     * Are we running on Windows?
-     */
-    static final boolean IS_WINDOWS_OS = System.getProperty("os.name").toUpperCase()
-            .contains("WINDOWS");
+
+	@Test
+	public void testMicrosoft() throws FileNotFoundException, IOException, ParseException {
+		ExtractJmeterParams extractor = new ExtractJmeterParams();
+		extractor.setType(TYPE.MICROSOFT);
+		extractor.setSeperator(";");
+		extractor.setColumns(new String[] { "serviceAction", "transparent", "requesttype", "Basemap" });
+		String obs = extractor.getSeperator();
+		assertEquals(";", obs);
+		extractor.parseFile(ms.getAbsolutePath());
+		ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+		PrintStream out = new PrintStream(bytes);
+		extractor.print("URL", "print", out);
+
+		byte[] b = bytes.toByteArray();
+		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(b);
+		BufferedReader r = new BufferedReader(new InputStreamReader(byteArrayInputStream), 32);
+		String header = r.readLine();
+		assertEquals("URL;SERVICEACTION;TRANSPARENT;REQUESTTYPE;BASEMAP;", header);
+		// serviceAction=ZoomToLocation&transparent=true&requesttype=Map&mapID=-1&Basemap=Essex%2Fbase_ADS_BandW&mapSource=Essex%2FECCGIS_Highways&layers=ecc_additions&northing=205195.6463425&easting=571411.246595&mapWidth=1670&mapHeight=932&mapResolution=0.25&mapScale=708.6618&units=m&zoom=417&maxExtent=1-1-700000-1300000&restrictedExtent=530000-170000-630000-250000&projection=EPSG%3A27700&dataLayerName=iSharemaps+Data+Layer
+		extractor.setColumns(new String[] { "requesttype", "Basemap", "mapSource", "layers", "northing", "easting",
+				"mapWidth", "mapHeight", "mapResolution", "mapScale", "zoom", "maxExtent", "restrictedExtent" });
+		extractor.parseFile(ms.getAbsolutePath());
+		bytes = new ByteArrayOutputStream();
+		out = new PrintStream(bytes);
+		extractor.print("URL", "print", out);
+		extractor.print("URL", "print");
+		b = bytes.toByteArray();
+		byteArrayInputStream = new ByteArrayInputStream(b);
+		r = new BufferedReader(new InputStreamReader(byteArrayInputStream), 32);
+		header = r.readLine();
+		assertEquals(
+				"URL;REQUESTTYPE;BASEMAP;MAPSOURCE;LAYERS;NORTHING;EASTING;MAPWIDTH;MAPHEIGHT;MAPRESOLUTION;MAPSCALE;ZOOM;MAXEXTENT;RESTRICTEDEXTENT;",
+				header);
+
+	}
+
+	/**
+	 * Takes a URL and converts it to a File. The attempts to deal with Windows
+	 * UNC format specific problems, specifically files located on network
+	 * shares and different drives.
+	 * <p>
+	 * If the URL.getAuthority() returns null or is empty, then only the url's
+	 * path property is used to construct the file. Otherwise, the authority is
+	 * prefixed before the path.
+	 * <p>
+	 * It is assumed that url.getProtocol returns "file".
+	 * <p>
+	 * Authority is the drive or network share the file is located on. Such as
+	 * "C:", "E:", "\\fooServer"
+	 * 
+	 * @param url
+	 *            a URL object that uses protocol "file"
+	 * @return a File that corresponds to the URL's location
+	 */
+	public static File urlToFile(URL url) {
+		if (!"file".equals(url.getProtocol())) {
+			return null; // not a File URL
+		}
+		String string = url.toString();
+		if (url.getQuery() != null) {
+			string = string.substring(0, string.indexOf("?"));
+		}
+		if (string.contains("+")) {
+			// this represents an invalid URL created using either
+			// file.toURL(); or
+			// file.toURI().toURL() on a specific version of Java 5 on Mac
+			string = string.replace("+", "%2B");
+		}
+		try {
+			string = URLDecoder.decode(string, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException("Could not decode the URL to UTF-8 format", e);
+		}
+		String path3;
+		String simplePrefix = "file:/";
+		String standardPrefix = "file://";
+		if (IS_WINDOWS_OS && string.startsWith(standardPrefix)) {
+			// win32: host/share reference. Keep the host slashes.
+			path3 = string.substring(standardPrefix.length() - 2);
+			File f = new File(path3);
+			if (!f.exists()) {
+				// Make path relative to be backwards compatible.
+				path3 = path3.substring(2, path3.length());
+			}
+		} else if (string.startsWith(standardPrefix)) {
+			path3 = string.substring(standardPrefix.length());
+		} else if (string.startsWith(simplePrefix)) {
+			path3 = string.substring(simplePrefix.length() - 1);
+		} else {
+			String auth = url.getAuthority();
+			String path2 = url.getPath().replace("%20", " ");
+			if (auth != null && !auth.equals("")) {
+				path3 = "//" + auth + path2;
+			} else {
+				path3 = path2;
+			}
+		}
+		return new File(path3);
+	}
+
+	/**
+	 * Are we running on Windows?
+	 */
+	static final boolean IS_WINDOWS_OS = System.getProperty("os.name").toUpperCase().contains("WINDOWS");
 
 }
